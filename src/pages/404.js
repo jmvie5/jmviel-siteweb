4efebb60de200b8fa445eaa77235abace64b567a
jmvie5 <jeanmichel.viel@gmail.com>
2023-06-01 19:09:43 -0400

    fix build

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,10 +8,11 @@ const NotFoundPage = () => {
 
 return (
     <Layout pageTitle="Erreur 404" description="Page introuvable.">
+        <title>404</title>
         <div className="space-y-4">
             <p className="text-jmv_white">Page introuvable.</p>
-            <Link to="/" className="flex items-center gap-2 bg-jmv_white hover:bg-gray-50 hover:ring-1 hover:ring-jmv_light active:bg-jmv_white focus:outline-none focus:ring-1 focus:ring-jmv_light rounded-md w-40 p-2">
-                <ArrowSmallLeftIcon className="w-10 h-10"/>
+            <Link to="/" className="flex items-center gap-2 bg-jmv_white hover:bg-gray-50 hover:ring-1 hover:ring-jmv_light active:bg-jmv_white focus:outline-none focus:ring-1 focus:ring-jmv_light rounded-md w-32 p-2">
+                <ArrowSmallLeftIcon className="w-12 h-12"/>
                 <p>Retour à l'accueil</p>
             </Link>
         </div>
--- src/pages/succes.js
+++ src/pages/succes.js
@@ -6,10 +6,11 @@ import {ArrowSmallLeftIcon} from "@heroicons/react/24/outline"
 const SuccessPage = () => {
     return (
         <Layout pageTitle="Merci!">
+            <title>Merci!</title>
             <div className="space-y-4">
                 <p className="text-jmv_white">Votre message a été envoyé avec succès. Je vous répondrai dans les plus brefs délais.</p>
-                <Link to="/" className="flex items-center gap-2 bg-jmv_white hover:bg-gray-50 hover:ring-1 hover:ring-jmv_light active:bg-jmv_white focus:outline-none focus:ring-1 focus:ring-jmv_light rounded-md w-24 p-2">
-                    <ArrowSmallLeftIcon className="w-6 h-6"/>
+                <Link to="/" className="flex items-center gap-2 bg-jmv_white hover:bg-gray-50 hover:ring-1 hover:ring-jmv_light active:bg-jmv_white focus:outline-none focus:ring-1 focus:ring-jmv_light rounded-md w-32 p-2">
+                    <ArrowSmallLeftIcon className="w-12 h-12"/>
                     <p className="mr-1">Retour à l'accueil</p>
                 </Link>
             </div>
